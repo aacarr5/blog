47667feac0f2e8e769015c93ec64dde00462bf0f
Andrew Carr <aacarr5@gmail.com>
2015-08-16 21:26:44 -0500

    Whatever styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,23 @@ class PostsController < ApplicationController
 	  end
   end
 
+  def update
+    @post = Post.find(params[:id])
+ 
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  	@post = Post.find(params[:id])
+  	@post.destroy
+
+  	redirect_to posts_path
+  end
+
  
   private
  
